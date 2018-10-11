1b12ddba47290026ab1d215ccdb2297a3290227f
Högberg Mats (Consultant) <u043439@corp.saab.se>
2018-10-11 08:54:13 +0200

    Added comments

--- routes/routes.js
+++ routes/routes.js
@@ -1,29 +1,35 @@
 var dataController = require("../controllers/dataController.js");
 
+/**
+ * The routes.
+ * 
+ */
 var appRouter = function(app) {
     /**
-     * Route for get requests with query string data
+     * Route for get requests with query string data,
+     * like  http://url:port?sensorid=1&value=2[&pushthru=true]
      */
     app.get("/", function(req, res) {
         dataController.handleUpdateQuery(req,res);
     });
 
     /**
-     * Route for get requests with 2 parameters
+     * Route for get requests with 2 parameters,
+     * like http://url:port/sensorid/value
      */
     app.get("/:sensorid/:value", function(req, res){
-        // res.send({"Device": req.params.deviceId, "Value": req.params.value});
         dataController.handleUpdate(req,res);
     });
 
     /**
-     * Route for get requests with 3 parameters
+     * Route for get requests with 3 parameters,
+     * like http://url:port/sensorid/value/true
      */
     app.get("/:sensorid/:value/:pushthru", function(req, res){
         dataController.handleUpdate(req,res);
     });
     /**
-     * Route for posted data
+     * Route for posted (x-www-form-urlencoded) data
      */
     app.post("/", function(req, res){
         dataController.handleUpdatePost(req,res);
